45d26416c8ad46aa4e18ef4c42628ef5a8981c1c
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-11-05 16:00:45 -0700

    hide/show,add/remove optional/mandatory select options

--- src/js/modules/view/dimensionspane.js
+++ src/js/modules/view/dimensionspane.js
@@ -21,27 +21,48 @@ function($, uiconfig, ui) {
     function updateDataSource(source) {
       datasource = source;
       var $select = $fields.find("select");
-      $select.find("option.value").remove();
+      $select.find(".value").remove();
       $select.attr("disabled", false);
+      if($select.find(".optional").length === 0)
+        $select.prepend('<option value="" class="mandatory">[select]</option>');
       $(source.fields.list).each(function() {
         var $option = $('<option class="value '+this.type+'">'+this.name+'</option>');
         $select.append($option);
       });
     }
 
-    function appendDimension(info) {
-      $fields.append('<div class="name">'+(info.label || info.name)+'</div>');
-      var $div = $('<div class="dimension"></div>').appendTo($fields),
-          $select = $('<select></select>').appendTo($div);
+    function createSelect($container, info) {
+      var $select = $('<select></select>').appendTo($container);
       $select.attr("disabled", true);
       if(info.min === 0) {
         $select.append('<option value="" class="optional">[optional]</option>');
       } else {
         $select.append('<option value="" class="mandatory">[select]</option>');
       }
+      var lastval;
       $select.on("change", function() {
-        ctx.trigger("chart.field.add", datasource.fields.map[$(this).val()], info);
+        var val = $(this).val();
+        if(val == "") {
+          ctx.trigger("chart.field.remove", datasource.fields.map[lastval], info)
+        } else {
+          ctx.trigger("chart.field.add", datasource.fields.map[val], info);
+        }
+        if($(this).val() == "") {
+          $select.find(".optional").text("[optional]");
+        } else {
+          $select.find(".optional").text("[remove]");
+        }
+        $select.find(".mandatory").remove();
+        lastval = val;
       });
+
+      return $select;
+    }
+
+    function appendDimension(info) {
+      $fields.append('<div class="name">'+(info.label || info.name)+'</div>');
+      var $div = $('<div class="dimension"></div>').appendTo($fields);
+      createSelect($div, info);
     }
 
     function init(container) {
