f6a2dce922951b56827409db3fb6c6c36bfa6c13
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-12-10 12:28:32 -0700

    fix for relative urls

--- src/js/lib/model/exports/exporturl.js
+++ src/js/lib/model/exports/exporturl.js
@@ -3,7 +3,17 @@ define([
 ],
 
 function() {
+
   return function(datasource) {
-    return 'ReportGrid.request("'+datasource.src+'")';
+    var src = datasource.src,
+        hasprotocol = false;
+    ["http://", "https://", "file:"].forEach(function(v) { if(src.substr(0, v.length) === v) hasprotocol = true; })
+    if(!hasprotocol) {
+      var base = window.location.href.split("/").slice(0,-1).join("/");
+      if(base.substr(-1) !== "/" && src.substr(0, 1) !== "/")
+        base += "/";
+      src = base + src;
+    }
+    return 'ReportGrid.request("'+src+'")';
   };
 });
