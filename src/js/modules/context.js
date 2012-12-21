8a8ff23157980fca4cf1497c7b31ad11fcb38e2c
Franco Ponticelli <franco.ponticelli@gmail.com>
2012-12-21 16:27:53 -0700

    improving debug

--- src/js/modules/context.js
+++ src/js/modules/context.js
@@ -1,11 +1,12 @@
 define([
-  "lib/util/dispatcher"
+    "jquery"
+  , "lib/util/dispatcher"
 ],
-function(createDispatcher) {
+function($, createDispatcher) {
   var logger = window.console || {info:function(){}};
   var log = function() {
-        logger.groupCollapsed(arguments[0]);
-        [new Date().toLocaleTimeString()].concat($.makeArray(arguments).slice(1)).forEach(function(v) {
+        logger.groupCollapsed("%c"+arguments[0]+": "+arguments[1], 'background: #222; color: #bada55');
+        [new Date().toLocaleTimeString()].concat($.makeArray(arguments).slice(2)).forEach(function(v) {
           logger.info(v);
         });
         logger.groupEnd();
@@ -18,17 +19,60 @@ function(createDispatcher) {
 
     // DEBUG
     if(debug) {
-      var trigger = ctx.trigger;
-      ctx.counter = {};
+      var counter = {},
+          states  = {};
+
+      var generic_extractor = function(name, args) {
+            return [name, args[0], args.slice(1)];
+          },
+          methods = [
+              { name    : "trigger" }
+            , { name    : "provide" }
+            , { name    : "on" }
+//            , { name    : "one" }
+            , { name    : "off", extract: function(name, args) { return [name]; } }
+            , { name    : "when", extract: function(name, args) { return [name]; } }
+            , { name    : "request", extract: function(name, args) { return [name]; } }
+            , { name    : "respond", extract: function(name, args) { return [name]; } }
+          ];
+
+      for(var i = 0; i < methods.length; i++) {
+        (function(method) {
+          var name     = method.name,
+              extract  = method.extract || generic_extractor,
+              original = ctx[name];
+          ctx[name] = function() {
+            var args = extract(name, $.makeArray(arguments));
+            var type = (ctx.counter[args[0]] || (ctx.counter[args[0]] = {}));
+            type[args[1]] = (type[args[1]] || 0) + 1;
+            log.apply(window, args);
+            original.apply(ctx, arguments);
+          }
+        })(methods[i]);
+      }
+      /*
       ctx.trigger = function() {
         var type = arguments[0];
         ctx.counter[type] = (ctx.counter[type] || 0) + 1;
         log.apply(window, arguments);
         trigger.apply(ctx, arguments);
       };
+
+      var provide = ctx.provide;
+      ctx.provide = function() {
+        var type = arguments[0];
+        ctx.counter[type] = (ctx.counter[type] || 0) + 1;
+        log.apply(window, arguments);
+        provide.apply(ctx, arguments);
+      };
+*/
       ctx.log = function() {
         log.apply(window, arguments);
       };
+
+      ctx.counter = function() {
+        return counter;
+      }
     } else {
       ctx.log = function() {};
     }
--- src/js/modules/model/chartbuilder.js
+++ src/js/modules/model/chartbuilder.js
@@ -59,7 +59,7 @@ function(charts, createLoader) {
         if(axes === null)
           throw "not enough axes to feed the chart";
         charts.map[current.type].extractOptions(options, current.dimensions, current.options);
-        ctx.log("CHART OPTIONS", JSON.stringify(options));
+        ctx.log("chart", "options", JSON.stringify(options));
         ctx.trigger("chart.render.execute", { type : current.type, loader : loader, datasource : current.datasource, axes : axes, options : options, customcss : current.options["css.palette.set"] });
       } catch(e) {
         ctx.trigger("chart.render.clear");
