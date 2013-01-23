92d8f69219c78fd111b12dd44b2d6cbc39059a38
Franco Ponticelli <franco.ponticelli@gmail.com>
2013-01-23 14:15:48 -0700

    changed Pardot page actions url

--- src/js/modules/service/pardot.js
+++ src/js/modules/service/pardot.js
@@ -4,7 +4,7 @@ define([
 
 function(pardot) {
   var import_done = function import_done() {
-    pardot.track_page("upload_chart");
+    pardot.track_page("upload");
   };
 
   return function(ctx) {
@@ -16,7 +16,7 @@ function(pardot) {
 
     // FILE DOWNLOAD
     ctx.on("reports.report.export", function() {
-      pardot.track_page("download_chart");
+      pardot.track_page("download");
     });
 
     // OPEN REPORT
@@ -26,33 +26,33 @@ function(pardot) {
 
     // DOWNLOAD HTML
     ctx.on("chart.export.html", function() {
-      pardot.track_page("export_html");
+      pardot.track_page("export");
     });
 
     // CHANGE CHART TYPE
     ctx.on("user.chart.type", function() {
-      pardot.track_page("choose_chart_type");
+      pardot.track_page("chart_type");
     });
 
     // CHOOSE CHART DATASOURCE
     ctx.on("user.chart.datasource", function() {
-      pardot.track_page("choose_datasource");
+      pardot.track_page("datasource");
     });
 
     // CHOOSE CHART DIMENSION
     ctx.on("user.chart.dimension", function() {
-      pardot.track_page("choose_dimension");
+      pardot.track_page("dimensions");
     });
 
     // CHANGE CHART OPTION
     ctx.on("user.chart.option", function() {
-      pardot.track_page("change_option");
+      pardot.track_page("options");
     });
 
 
     // ADD DATASOURCE
     ctx.on("user.datasource.add", function() {
-      pardot.track_page("add_datasource");
+      pardot.track_page("add_data_source");
     });
 
     // FORM HANDLERS
